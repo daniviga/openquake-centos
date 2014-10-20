41eba2cd9acc9f50f5f1ec4013dd2082c1b9de98
Daniele Vigano <daniele@vigano.me>
2014-10-20 17:18:39 +0200

    Disable debug

--- bin/deploy-openquake-centos6.sh
+++ bin/deploy-openquake-centos6.sh
@@ -16,7 +16,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-set -x
+# Uncomment to enable debug
+# set -x
 set -e
 set -o pipefail
 
